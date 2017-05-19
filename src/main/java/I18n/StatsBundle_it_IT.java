7117686a690dbe3306b266b7cea4961742c5e9b9
SlyF0Xx <max.tolstukhin@mail.ru>
2017-05-19 23:02:59 +0300

    Prepare for writing 8 lab. Create locale classes. Prepare for creating class which extended by SimpleStringProperty

---
+++ src/main/java/I18n/StatsBundle_it_IT.java
@@ -0,0 +1,13 @@
+package I18n;
+
+import java.util.ListResourceBundle;
+
+/**
+ * Created by SlyFox on 19.05.2017.
+ */
+public class StatsBundle_it_IT extends ListResourceBundle {
+    @Override
+    protected Object[][] getContents() {
+        return new Object[0][];
+    }
+}
